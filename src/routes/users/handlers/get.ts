cd352e0ea6be17224c29c6ad33f9bf4105cae6ce
Yury Kurouski <yury_kurouski@epam.com>
2024-02-10 20:33:17 +0100

    feat: add uuid validation

--- src/routes/users/handlers/get.ts
+++ src/routes/users/handlers/get.ts
@@ -1,5 +1,7 @@
 import { IncomingMessage } from 'http';
+import { validate } from 'uuid';
 
+import { MESSAGE_INVALID_ID, MESSAGE_NO_USER } from '../../../constants/index.ts';
 import UserStore from '../../../store/index.ts';
 import { TServerResponse } from '../../../types/index.ts';
 import { parseReqParams, sendData } from "../../../utils/index.ts";
@@ -13,8 +15,18 @@ export const handleGet = (
   if (params?.length) {
     const userID = params[0]!;
 
-    const user = UserStore.getUser(userID);
-    sendData(res, user, 200);
+    if (validate(userID)) {
+      const user = UserStore.getUser(userID);
+
+      if (user) {
+        return sendData(res, user, 200);
+      } else {
+        return sendData(res, MESSAGE_NO_USER, 404);
+      }
+
+    } else {
+      sendData(res, MESSAGE_INVALID_ID, 400);
+    }
   } else {
     const users = UserStore.getUsers();
 
