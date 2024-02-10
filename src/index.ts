97f9920b7b09a81f416982d6ccd212620bd28e8d
Yury Kurouski <yury_kurouski@epam.com>
2024-02-10 22:42:58 +0100

    feat: port number from env

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
+require('dotenv').config();
+
 import http from "http";
 
 import { router } from "./router/router.ts";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 export const server = http.createServer();
+console.log(process.env);
+if (PORT) {
+  server.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
 
-server.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
-});
+  server.on("request", router);
+} else {
+  console.error("Provide a port!");
+}
 
-server.on("request", router);
