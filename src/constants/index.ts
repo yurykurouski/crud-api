1cd91bea9abb347fb3070e3c207c394beb1675f1
Yury Kurouski <yury_kurouski@epam.com>
2024-02-10 20:13:05 +0100

    feat: add constats

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,10 @@ export const METHOD_GET = 'GET';
 export const METHOD_PUT = 'PUT';
 export const METHOD_POST = 'POST';
 export const METHOD_DELETE = 'DELETE';
+
+export const URL_PATH_USERS = 'users';
+
+export const ALLOWED_USER_FIELDS = ['username', 'age', 'hobbies', 'id'];
+
+export const MESSAGE_WRONG_USER_DATA = 'Wrong user data';
+export const MESSAGE_INVALID_PARAM = 'Invalid param';
